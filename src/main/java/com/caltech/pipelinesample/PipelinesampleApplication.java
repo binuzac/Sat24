f5e5b3fab5ccbcd973df5be9b03084b8713f3cd9
student <binuzac@gmail.com>
2021-04-24 16:27:21 +0000

    Pipleline Example

---
+++ src/main/java/com/caltech/pipelinesample/PipelinesampleApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.pipelinesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinesampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinesampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/pipelinesample/PipelinesampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.pipelinesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinesampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
